97d73669f1c04ddac0cb57322877ddc2d4a7c60d
hmzunal <88240004+hmzunal@users.noreply.github.com>
2024-11-17 09:33:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function london(){
+    document.getElementById("london").style.display="block";
+    document.getElementById("corum").style.display="none";
+    document.getElementById("ist").style.display="none"
+}
+
+function corum(){
+    document.getElementById("corum").style.display="block";
+    document.getElementById("london").style.display="none";
+    document.getElementById("ist").style.display="none"
+}
+
+function istanbul(){
+    document.getElementById("ist").style.display="block";
+    document.getElementById("corum").style.display="none";
+    document.getElementById("london").style.display="none"
+}
